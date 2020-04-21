e42c3c356ec7151898455ae170bdd1fba1323ea9
highmoonrays <steachy.hm.123@gmail.com>
2020-04-21 21:52:13 +0300

    working with environment and look of structure, just begin adding some views, and model, and also controllers

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>Welcome to the blog</h1>
+@endsection
+
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'PagesController@index');
+
+Route::get('/services', 'PagesController@services');
+
+Route::resource('Posts', 'PostsController');
+
